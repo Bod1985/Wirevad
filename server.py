067e7e5ffcb6ce8bfbf77a1bf52a573157bcbd9c
Bod1985 <crwblyth@gmail.com>
2023-03-28 22:00:00 +0100

    Update server.py

--- server.py
+++ server.py
@@ -5,69 +5,55 @@ import subprocess
 import qrcode
 import http.server
 import socketserver
+import base64
 
-MULLVAD_ADDRESS = os.env.get('MULLVAD_ADDRESS')
-FWMARK = os.env.get('FWMARK')
-MULLVAD_PRIVATEKEY = os.env.get('MULLVAD_PRIVATEKEY')
-MULLVAD_DNS = os.env.get('MULLVAD_DNS')
+MULLVAD_ADDRESS = os.environ.get('MULLVAD_ADDRESS')
+FWMARK = os.environ.get('FWMARK')
+MULLVAD_PRIVATEKEY = os.environ.get('MULLVAD_PRIVATEKEY')
+MULLVAD_DNS = os.environ.get('MULLVAD_DNS')
 
-MULLVAD_PUBLICKEY = os.env.get('MULLVAD_PUBLICKEY')
-MULLVAD_ENDPOINT = os.env.get('MULLVAD_ENDPOINT')
-LAN_SUBNET = os.env.get('LAN_SUBNET')
-PORT = os.env.get('PORT')
-INTERFACE = os.env.get('INTERFACE')
-NUMBER_OF_CLIENTS = os.env.get('NUMBER_OF_CLIENTS')
-DNS_SERVER = os.env.get('DNS_SERVER')
-DOMAIN = os.env.get('DOMAIN')
+MULLVAD_PUBLICKEY = os.environ.get('MULLVAD_PUBLICKEY')
+MULLVAD_ENDPOINT = os.environ.get('MULLVAD_ENDPOINT')
+LAN_SUBNET = os.environ.get('LAN_SUBNET')
+PORT = os.environ.get('PORT')
+INTERFACE = os.environ.get('INTERFACE')
+NUMBER_OF_CLIENTS = os.environ.get('NUMBER_OF_CLIENTS')
+DNS_SERVER = os.environ.get('DNS_SERVER')
+DOMAIN = os.environ.get('DOMAIN')
 
 
 os.chdir('/opt/wirevad')
 
-FILE = '/etc/wireguard/wirevadmullvad.conf'
-if os.path.isfile(FILE):
-    print(f'{FILE} exists. Bringing down interface as a precaution.')
-    subprocess.run(['wg-quick', 'down', 'wirevadmullvad'])
+def wg_down(interface):
+  subprocess.run(['wg-quick', 'down', interface])
 
-FILE = '/etc/wireguard/wirevadhost.conf'
-if os.path.isfile(FILE):
-    print(f'{FILE} exists. Bringing down interface as a precaution.')
-    subprocess.run(['wg-quick', 'down', 'wirevadhost'])
+def wg_up(interface):
+  subprocess.run(['wg-quick', 'up', interface])
 
-time.sleep(1)
+def wg_createmullvad():
+  with open('/opt/wirevad/wirevadmullvad.conf', 'w') as f:
+      f.write(f"""
+      [Interface]
+      Address = {MULLVAD_ADDRESS}
+      FwMark = {FWMARK}
+      PrivateKey = {MULLVAD_PRIVATEKEY}
+      DNS = {MULLVAD_DNS}
 
-FILE = '/opt/wirevad/wirevadmullvad.conf'
-if os.path.isfile(FILE):
-    print(f'{FILE} exists. Skipping creation of new keys & config.')
-else:
-    print(f'{FILE} does not exist. Creating new keys & config.')
-    with open('/opt/wirevad/wirevadmullvad.conf', 'w') as f:
-        f.write(f"""
-        [Interface]
-        Address = {MULLVAD_ADDRESS}
-        FwMark = {FWMARK}
-        PrivateKey = {MULLVAD_PRIVATEKEY}
-        DNS = {MULLVAD_DNS}
-
-        PostUp  = iptables -t mangle -A OUTPUT -d 10.10.12.0/24,{LAN_SUBNET} -j MARK --set-mark 51820
-        PreDown = iptables -t mangle -D OUTPUT -d 10.10.12.0/24,{LAN_SUBNET} -j MARK --set-mark 51820
-        PostUp  = iptables -I OUTPUT ! -o %i -m mark ! --mark 51820 -m addrtype ! --dst-type LOCAL -j REJECT
-        PreDown = iptables -D OUTPUT ! -o %i -m mark ! --mark 51820 -m addrtype ! --dst-type LOCAL -j REJECT
-        PostUp =  ip route add {LAN_SUBNET} via $(ip route | grep default | awk "{{print $3}}"); iptables -I OUTPUT -d {LAN_SUBNET} -j ACCEPT
-        PreDown = ip route del {LAN_SUBNET} via $(ip route | grep default | awk "{{print $3}}"); iptables -D OUTPUT -d {LAN_SUBNET} -j ACCEPT
-        
-        [Peer]
-        # Phone
-        PublicKey = {MULLVAD_PUBLICKEY}
-        AllowedIPs = 0.0.0.0/0
-        Endpoint = {MULLVAD_ENDPOINT}')
-        """)
-
-FILE = "/opt/wirevad/wirevadhost.conf"
-
-if os.path.isfile(FILE):
-    print(FILE + " exists. Skipping creation of new keys & config.")
-else:
-    print(FILE + " does not exist. Creating new keys & config.")
+      PostUp  = iptables -t mangle -A OUTPUT -d 10.10.12.0/24,{LAN_SUBNET} -j MARK --set-mark 51820
+      PreDown = iptables -t mangle -D OUTPUT -d 10.10.12.0/24,{LAN_SUBNET} -j MARK --set-mark 51820
+      PostUp  = iptables -I OUTPUT ! -o %i -m mark ! --mark 51820 -m addrtype ! --dst-type LOCAL -j REJECT
+      PreDown = iptables -D OUTPUT ! -o %i -m mark ! --mark 51820 -m addrtype ! --dst-type LOCAL -j REJECT
+      PostUp =  ip route add {LAN_SUBNET} via $(ip route | grep default | awk '{{print $3}}'); iptables -I OUTPUT -d {LAN_SUBNET} -j ACCEPT
+      PreDown = ip route del {LAN_SUBNET} via $(ip route | grep default | awk '{{print $3}}'); iptables -D OUTPUT -d {LAN_SUBNET} -j ACCEPT
+      
+      [Peer]
+      # Phone
+      PublicKey = {MULLVAD_PUBLICKEY}
+      AllowedIPs = 0.0.0.0/0
+      Endpoint = {MULLVAD_ENDPOINT}
+      """)
+    
+def wg_createclientandpeers(num_of_clients):
     os.system("rm -f /opt/wirevadclient*.conf")
     os.system("sed -i 's/#net.ipv4.ip_forward=/net.ipv4.ip_forward=/' /etc/sysctl.conf")
     os.umask(0o77)
@@ -106,7 +92,7 @@ else:
         PreDown = iptables -t nat -D POSTROUTING -o wirevadmullvad -j MASQUERADE
         """)
 
-    for i in range(1, NUMBER_OF_CLIENTS + 1):
+    for i in range(1, int(num_of_clients) + 1):
         private_key = os.popen('wg genkey').read().strip()
         with open('privatekey_client', 'w') as f:
             f.write(private_key)
@@ -143,6 +129,158 @@ else:
 
     print("Your WireGuard config files are located at /opt/wirevad/- don't forget to update your client devices with the new config!")
 
+def createHTML():
+  # Define the directory containing the configuration files
+  config_dir = '/opt/wirevad/'
+
+  qr_codes = []
+  for file_name in os.listdir(config_dir):
+      if file_name.endswith('.conf') and file_name.startswith('wirevadclient'):
+          file_path = os.path.join(config_dir, file_name)
+          with open(file_path, 'rb') as f:
+              config_data = f.read()
+              qr_code = qrcode.make(config_data)
+              image_name = f'{file_name}.png'
+              qr_code.save(image_name)
+              with open(file_path, 'rb') as f:
+                  data_uri = base64.b64encode(f.read()).decode('utf-8')
+                  download_link = f"data:application/octet-stream;base64,{data_uri}"
+              qr_codes.append({'name': file_name, 'image': image_name, 'link': download_link})
+  qr_codes.sort(key=lambda x: x['name'])
+
+  css = f'''
+  body {{
+    background-color: #1C1C1E;
+    color: #FFFFFF;
+    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+    font-size: 16px;
+    line-height: 1.6;
+    margin: 0;
+    padding: 0;
+  }}
+
+  h1 {{
+    color: #FFFFFF;
+    font-size: 28px;
+    font-weight: bold;
+    margin: 0;
+    padding: 20px 0;
+    text-align: center;
+    text-shadow: 0 1px 2px rgba(0,0,0,0.5);
+  }}
+
+  .qr-codes-container {{
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin: 0;
+    padding: 20px;
+  }}
+
+  .qr-code {{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 20px;
+    padding: 20px;
+    background-color: #2C2C2E;
+    border-radius: 10px;
+    box-shadow: 0 2px 4px rgba(0,0,0,0.08);
+    transition: box-shadow 0.2s ease-in-out;
+  }}
+
+  .qr-code:hover {{
+    box-shadow: 0 4px 8px rgba(0,0,0,0.12);
+  }}
+
+  .qr-code img {{
+    width: 200px;
+    height: 200px;
+    margin-bottom: 10px;
+  }}
+
+  .qr-code span {{
+    color: #FFFFFF;
+    font-size: 18px;
+    font-weight: bold;
+    text-align: center;
+    text-shadow: 0 1px 2px rgba(0,0,0,0.5);
+  }}
+
+  .qr-code .download-link {{
+    display: inline-block;
+    background-color: #428bca;
+    color: white;
+    padding: 8px 12px;
+    border-radius: 5px;
+    text-decoration: none;
+    margin-top: 10px;
+    transition: background-color 0.2s ease-in-out;
+  }}
+
+  .qr-code .download-link:hover {{
+    background-color: #3071a9;
+  }}
+  '''
+  with open('style.css', 'w') as f:
+      f.write(css)
+
+  html = f'''
+  <!DOCTYPE html>
+  <html>
+  <head>
+    <title>QR Codes</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+    <h1>Wirevad</h1>
+    
+    <div class="qr-codes-container">
+  '''
+
+  for qr_code in qr_codes:
+      html += f'''
+      <div class="qr-code">
+        <img src="{qr_code['image']}" alt="{qr_code['name']} QR code">
+        <span>{qr_code['name']}</span>
+        <a href="{qr_code['name']}" class="download-link">Download</a>
+      </div>
+      '''
+  html += f'''
+    </div>
+  </body>
+  </html>
+  '''
+
+  with open('index.html', 'w') as f:
+      f.write(html)
+
+FILE = '/etc/wireguard/wirevadmullvad.conf'
+if os.path.isfile(FILE):
+    print(f'{FILE} exists. Bringing down interface as a precaution.')
+    wg_down('wirevadmullvad')
+
+FILE = '/etc/wireguard/wirevadhost.conf'
+if os.path.isfile(FILE):
+    print(f'{FILE} exists. Bringing down interface as a precaution.')
+    wg_down('wirevadhost')
+
+FILE = '/opt/wirevad/wirevadmullvad.conf'
+if os.path.isfile(FILE):
+    print(f'{FILE} exists. Skipping creation of new keys & config.')
+else:
+    print(f'{FILE} does not exist. Creating new keys & config.')
+    wg_createmullvad()
+
+FILE = "/opt/wirevad/wirevadhost.conf"
+
+if os.path.isfile(FILE):
+    print(FILE + " exists. Skipping creation of new keys & config.")
+else:
+    print(FILE + " does not exist. Creating new keys & config.")
+    wg_createclientandpeers(NUMBER_OF_CLIENTS)
+
 # Reload sysctl configuration
 subprocess.run(['sysctl', '-p'])
 
@@ -170,34 +308,17 @@ subprocess.run(['rm', '-f', '/opt/wirevad/privatekey_*'])
 subprocess.run(['chmod', '-R', '777', '/opt/wirevad'])
 
 
-# Define the directory containing the configuration files
-config_dir = '/opt/wirevad/'
-
-# Generate a QR code for each configuration file in the directory
-qr_codes = []
-for file_name in os.listdir(config_dir):
-    if file_name.endswith('.conf'):
-        file_path = os.path.join(config_dir, file_name)
-        with open(file_path, 'r') as f:
-            config_data = f.read()
-            qr_code = qrcode.make(config_data)
-            qr_codes.append(qr_code)
-
-# Define the HTML code for displaying the QR codes in a simple web interface
-html = '<html><body>'
-for i, qr_code in enumerate(qr_codes):
-    image_name = f'image_{i}.png'
-    qr_code.save(image_name)
-    html += f'<img src="{image_name}" /><br>'
-html += '</body></html>'
-
-# Save the HTML code to a file
-with open('index.html', 'w') as f:
-    f.write(html)
-
+createHTML()
 # Serve the HTML file and the QR code images with a local web server
 PORT = 8000
 Handler = http.server.SimpleHTTPRequestHandler
 httpd = socketserver.TCPServer(("", PORT), Handler)
 print(f"Serving on port {PORT}")
-httpd.serve_forever()
+
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    pass
+
+httpd.server_close()
+print("Server stopped.")
